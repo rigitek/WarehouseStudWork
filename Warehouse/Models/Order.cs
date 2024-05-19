bd36341def0bdbab62dddb76182f3b39fd07c052
rigitek <rustemmukhametdinov000@gmail.com>
2024-05-19 22:46:06 +0500

    upd

---
+++ Warehouse/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ Warehouse/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace Warehouse.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Title {  get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+    }
+}
