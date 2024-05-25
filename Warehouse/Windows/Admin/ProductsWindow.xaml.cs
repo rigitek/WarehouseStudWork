f14dad538f78b53e8e2e66d5130994e563bc3655
rigitek <rustemmukhametdinov000@gmail.com>
2024-05-25 15:18:17 +0500

    upd

--- Warehouse/Windows/Admin/ProductsWindow.xaml.cs
+++ Warehouse/Windows/Admin/ProductsWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -48,6 +49,46 @@ namespace Warehouse.Windows.Admin
                 db.SaveChanges();
             }
         }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Product? product = productsGrid.SelectedItem as Product;
+            if (product is null) return;
+
+            AddProductWindow AddProductWindow = new AddProductWindow(new Product
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Amount = product.Amount,
+                Price = product.Price
+            });
+
+
+            if (AddProductWindow.ShowDialog() == true)
+            {
+                // получаем измененный объект
+                product = db.Products.Find(AddProductWindow.Product.Id);
+                if (product != null)
+                {
+                    product.Title = AddProductWindow.Product.Title;
+                    product.Amount = AddProductWindow.Product.Amount;
+                    product.Price = AddProductWindow.Product.Price;
+                    db.SaveChanges();
+                    productsGrid.Items.Refresh();
+                }
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            Product? product = productsGrid.SelectedItem as Product;
+            // если ни одного объекта не выделено, выходим
+            if (product is null) return;
+            db.Products.Remove(product);
+            db.SaveChanges();
+        }
+
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
